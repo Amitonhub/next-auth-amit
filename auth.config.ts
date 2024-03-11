7edf8f5239586094760c5804aac8ffc7c798e927
Amit <amitthakurat4615321@gmail.com>
2024-03-11 21:31:39 +0530

    minor change

--- auth.config.ts
+++ auth.config.ts
@@ -4,7 +4,7 @@ import { LoginSchema } from "@/schemas"
 import { getUserByEmail } from "@/data/user"
 import bcrypt from 'bcryptjs'
 import Github from 'next-auth/providers/github'
-import Google from 'next-auth/providers/Google'
+import Google from 'next-auth/providers/google'
 
 export default {
   providers: [
